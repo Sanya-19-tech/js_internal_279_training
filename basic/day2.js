cf403c2406617588227273035fba846c32c4ead7
Sanya-19-tech <sanyashukla04@gmail.com>
2025-12-09 05:33:59 +0000

    day2 task

---
+++ basic/day2.js
@@ -0,0 +1,26 @@
+// //let a=23;
+// console.log(a);
+// const b="abc";
+// console.log(b);
+// var c=78;
+// console.log(c);
+// d=88;
+// console.log(d);
+// let e="abc";
+// console.log(e);
+// let f="";
+// console.log(f);
+// let g=null;
+// console.log(g);
+// let h;
+// console.log(h);
+// console.log(typeof (a));
+// console.log(typeof(undefined));
+// console.log(typeof(null));//
+
+
+let score="33";
+console.log(typeof(score));
+console.log(typeof score);
+let scores=Number(score);
+console.log(typeof(scores));
